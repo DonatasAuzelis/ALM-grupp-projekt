adc5da3804e93e6d48283e5763ef425e23727966
Donatas Auzelis <donatas963@gmail.com>
2021-05-18 10:08:26 +0200

    Added classes for Book

--- src/main/java/com/example/gruppprojekt/controller/BookController.java
+++ src/main/java/com/example/gruppprojekt/controller/BookController.java
@@ -1,4 +1,46 @@
 package com.example.gruppprojekt.controller;
 
+import com.example.gruppprojekt.model.Book;
+import com.example.gruppprojekt.repo.BookRepo;
+import com.example.gruppprojekt.service.BookService;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        return ResponseEntity.ok(bookService.addBook(book));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable String id) {
+        return ResponseEntity.ok(bookService.deleteBookById(id));
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Object> updateBook(@PathVariable String id, @RequestBody Book book) {
+
+        try {
+            return ResponseEntity.ok(bookService.updateBook(id, book));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
