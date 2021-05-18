0957abddda3b875570b9149e3c58a6849c1e9618
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-05-18 11:18:01 +0200

    Implement classes for User

--- src/main/java/com/example/gruppprojekt/model/Users.java
+++ src/main/java/com/example/gruppprojekt/model/Users.java
@@ -1,5 +1,11 @@
 package com.example.gruppprojekt.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * Created by Ashkan Amiri
  * Date:  2021-05-18
@@ -7,6 +13,13 @@ package com.example.gruppprojekt.model;
  * Project: ALM-grupp-projekt
  * Copyright: MIT
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "Users")
 public class Users extends Person{
 
+    @Id
+    private String id;
+    private Long personalNumber;
 }
