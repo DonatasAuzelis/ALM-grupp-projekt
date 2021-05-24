2169f0577be43ea35b72a86253290f974937979c
hodeiez <hodeinaiz@hotmail.com>
2021-05-24 23:50:05 +0200

    fixed tests and added a method to repo

--- src/test/java/com/example/gruppprojekt/repo/AuthorRepoTest.java
+++ src/test/java/com/example/gruppprojekt/repo/AuthorRepoTest.java
@@ -1,6 +1,7 @@
 package com.example.gruppprojekt.repo;
 
 import com.example.gruppprojekt.model.Author;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,12 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.ZoneOffset;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,9 +37,9 @@ class AuthorRepoTest {
     private List<Author> mockupList;
 
     @BeforeEach
-    public void setUp(){
-        Author author1=new Author("id1", LocalDateTime.now(),LocalDateTime.of(2021,6,7,0,0,0));
-        Author author2=new Author("id2", LocalDateTime.now(),LocalDateTime.of(2021,6,6,0,0,0));
+    public void setUp() {
+        Author author1 = new Author("id1", LocalDateTime.now(), LocalDateTime.now());
+        Author author2 = new Author("id2", LocalDateTime.now(), LocalDateTime.now());
 
         author1.setFirstName("ABC");
         author1.setLastName("ABC");
@@ -49,35 +49,68 @@ class AuthorRepoTest {
         author2.setDateOfBirth("1856-05-06");
 
 
-        mockupList= new ArrayList<>(Arrays.asList(author1, author2));
+        mockupList = new ArrayList<>(Arrays.asList(author1, author2));
         authorRepo.saveAll(mockupList);
     }
+
     @Test
     @DisplayName("If gives ordered last modified date is success")
     void findAllByOrderByLastModifiedDesc() {
+        //take from repository and get the ids as String to compare
+        List<Author> repolist = authorRepo.findAllByOrderByLastModifiedDateDesc();
+        List<String> actual = repolist.stream().map(Author::getId).collect(toList());
 
+        //Create a custom comparator, get from the ids from the list sorted by last modified date
+        Comparator<Author> byLastModifiedDate = Comparator.comparingLong(a -> a.getLastModifiedDate().toInstant(ZoneOffset.UTC).toEpochMilli());
+        List<String> expect = mockupList.stream()
+                .sorted(byLastModifiedDate.reversed())
+                .map(Author::getId)
+                .collect(toList());
 
+        //test if they are equals
+        assertEquals(expect, actual);
+
+        //--falsifiability->
+        //sort in Ascendant
+        expect = mockupList.stream()
+                .sorted(byLastModifiedDate)
+                .map(Author::getId)
+                .collect(toList());
+
+        //test they are not equals
+        assertNotEquals(expect, actual);
     }
+
     @Test
     @DisplayName("If gives ordered by name is success")
     void findAllByOrderByFirstNameDesc() {
-        List<Author>actual=authorRepo.findAll(Sort.by("firstName").descending());
-        List<Author> expect=mockupList;
-        assertEquals(expect,actual);
+        List<Author> repolist = authorRepo.findAll(Sort.by("firstName").descending());
+        List<String> actual = repolist.stream().map(Author::getFirstName).collect(toList());
+
+        List<String> expect = mockupList.stream().map(Author::getFirstName).collect(toList());
+
+        assertEquals(expect, actual);
+
         //--falsifiability->
-        expect=mockupList.stream().sorted(Comparator.comparing(Author::getFirstName)).collect(Collectors.toList());
-        assertNotEquals(expect,actual);
+        expect = mockupList.stream().sorted(Comparator.comparing(Author::getFirstName)).map(Author::getFirstName).collect(toList());
+        assertNotEquals(expect, actual);
+
+
     }
 
     @Test
     @DisplayName("If gives ordered by last name is success")
     void findAllByOrderByLastNameDesc() {
-        List<Author>actual=authorRepo.findAll(Sort.by("lastName").descending());
-        List<Author> expect=mockupList;
-        assertEquals(expect,actual);
+        List<Author> repolist = authorRepo.findAll(Sort.by("lastName").descending());
+        List<String> actual = repolist.stream().map(Author::getLastName).collect(toList());
+
+        List<String> expect = mockupList.stream().map(Author::getLastName).collect(toList());
+
+        assertEquals(expect, actual);
+
         //--falsifiability->
-        expect=mockupList.stream().sorted(Comparator.comparing(Author::getLastName)).collect(Collectors.toList());
-        assertNotEquals(expect,actual);
+        expect = mockupList.stream().sorted(Comparator.comparing(Author::getLastName)).map(Author::getLastName).collect(toList());
+        assertNotEquals(expect, actual);
 
     }
 
